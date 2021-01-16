3937db685663f700429dbaec2c2a28f5969e43a4
nordael <38379044+nordael@users.noreply.github.com>
2021-01-15 21:38:44 -0300

    Add files via upload

---
+++ Tunts/src/main/java/Main.java
@@ -0,0 +1,23 @@
+package tunts;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class Main {
+
+	public static void main(String... args) {
+		AverageGrades grades = new AverageGrades();
+		
+		try {
+			grades.updateAlumniStatus();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} catch (GeneralSecurityException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
